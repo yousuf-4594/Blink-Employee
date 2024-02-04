24d775820eed0fb0f6fff702e5340ad2fc504730
yousuf-4594 <cysiddiqui@gmail.com>
2024-02-04 21:43:26 +0500

    updated minSDK to 19 integrated firebase cli firebase connected to google account

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_restraunt/services/navigator.dart';
 import 'package:food_delivery_restraunt/screens/login_screen.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(FoodDelivery());
 }
 
